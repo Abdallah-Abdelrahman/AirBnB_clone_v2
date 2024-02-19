09abf1c82e55233a778153635e7f49eec0077919
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-19 16:36:27 +0300

    Fix typo in db name

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,5 +5,5 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Assign privileges
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
