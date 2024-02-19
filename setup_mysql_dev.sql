bcdb788b3bbb78621fff432420ca5e9b0e063c35
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-19 16:30:48 +0300

    Fix permissions on `performance_schema`

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,4 +6,4 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Assign privileges
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON 'performance_schema' TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
