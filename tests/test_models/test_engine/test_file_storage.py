3001763224a2427f6d3bb81b8f0e4c75644c59f0
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-02-19 17:15:25 +0200

    added pep8 and doc string tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -15,6 +15,45 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+import inspect
+import pycodestyle as pep8
+from models.engine import file_storage
+
+
+class TestBaseModelDocPep8(unittest.TestCase):
+    """unittest class for FileStorage class
+    documentation and pep8 conformaty"""
+    def test_pep8_base(self):
+        """Test that the base_module conforms to PEP8."""
+        style = pep8.StyleGuide()
+        result = style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_test_base(self):
+        """Test that the test_file_storage conforms to PEP8."""
+        style = pep8.StyleGuide()
+        result = style.check_files(['tests/test_models/test_engine/' +
+                                    'test_file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_module_docstring(self):
+        """test module documentation"""
+        mod_doc = file_storage.__doc__
+        self.assertTrue(len(mod_doc) > 0)
+
+    def test_class_docstring(self):
+        """test class documentation"""
+        mod_doc = str(FileStorage.__doc__)
+        self.assertTrue(len(mod_doc) > 0)
+
+    def test_func_docstrings(self):
+        """Tests for the presence of docstrings in all functions"""
+        base_funcs = inspect.getmembers(FileStorage, inspect.isfunction)
+        base_funcs.extend(inspect.getmembers(FileStorage, inspect.ismethod))
+        for func in base_funcs:
+            self.assertTrue(len(str(func[1].__doc__)) > 0)
 
 
 class Test_attributes(unittest.TestCase):
