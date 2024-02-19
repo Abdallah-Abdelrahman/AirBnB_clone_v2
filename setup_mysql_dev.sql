5887d6209d398d2175c017713d230c890805b9b3
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-19 16:16:23 +0300

    Prepare mysql server

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- Prepares a MySQL server FOR the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- CREATE USER
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Assign privileges
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON 'performance_schema' TO 'hbnb_dev'@'localhost';
