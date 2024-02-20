5925718eac544e4dcf03741e3df1796d2cf4edf2
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-02-20 18:20:29 +0200

    backpopulate, Amenities and places association table

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 '''Module defines Amenity'''
 
-from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base, store
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
+@store('place_amenities',
+       name=(Column(String(128), nullable=False), ''),
+       place_amenities=(relationship("Place",
+                        secondary="place_amenity",
+                        back_populates="amenities"),)
+       )
+class Amenity(BaseModel, Base):
     '''Amenity class.
 
     Atrrs:
         name: string
     '''
-    name = ''
+    __tablename__ = 'amenities'
--- models/place.py
+++ models/place.py
@@ -3,28 +3,38 @@
 
 from models.base_model import BaseModel, Base, store
 from models.review import Review
-from sqlalchemy import Column, Float, Integer, String, ForeignKey
+from models.amenity import Amenity
+from sqlalchemy import Table, Column, Float, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from typing import List
 
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'), primary_key=True)
+                      )
 
-@store('reviews',
-        city_id=(Column(String(60), ForeignKey('cities.id'),
-                        nullable=False), ''),
-        user_id=(Column(String(60), ForeignKey('users.id'),
-                        nullable=False), ''),
-        name=(Column(String(128), nullable=False), ''),
-        description=(Column(String(1024)), ''),
-        number_rooms=(Column(Integer, nullable=False, default=0), 0),
-        number_bathrooms=(Column(Integer, nullable=False, default=0), 0),
-        max_guest=(Column(Integer, nullable=False, default=0), 0),
-        price_by_night=(Column(Integer, nullable=False, default=0), 0),
-        latitude=(Column(Float), 0.0),
-        longitude=(Column(Float), 0.0),
-        reviews=(relationship('Review', backref='place',
-                              cascade='all, delete-orphan'), ),
-        amenity_ids=([], [])
-      )
+
+@store('reviews', 'amenities',
+       city_id=(Column(String(60), ForeignKey('cities.id'),
+                       nullable=False), ''),
+       user_id=(Column(String(60), ForeignKey('users.id'),
+                       nullable=False), ''),
+       name=(Column(String(128), nullable=False), ''),
+       description=(Column(String(1024)), ''),
+       number_rooms=(Column(Integer, nullable=False, default=0), 0),
+       number_bathrooms=(Column(Integer, nullable=False, default=0), 0),
+       max_guest=(Column(Integer, nullable=False, default=0), 0),
+       price_by_night=(Column(Integer, nullable=False, default=0), 0),
+       latitude=(Column(Float), 0.0),
+       longitude=(Column(Float), 0.0),
+       reviews=(relationship('Review', backref='place',
+                             cascade='all, delete-orphan'), ),
+       amenities=(relationship("Amenity", secondary=place_amenity,
+                               viewonly=False,
+                               back_populates='place_amenities'), [])
+       )
 class Place(BaseModel, Base):
     '''Place class.
 
@@ -50,3 +60,18 @@ class Place(BaseModel, Base):
         from models import storage
         return [v for v in storage.all(Review).values()
                 if v.place_id == self.id]
+
+    @property
+    def amenities(self) -> List:
+        """amenities getter attribute"""
+        from models import storage
+        return [v for v in storage.all(Amenity).values()
+                if v.id in self.amenity_ids]
+
+    @amenities.setter
+    def amenities(self, value: Amenity) -> None:
+        """amenities setter attribute"""
+        if isinstance(value, Amenity):
+            self.amenity_ids.append(value.id)
+        else:
+            pass
