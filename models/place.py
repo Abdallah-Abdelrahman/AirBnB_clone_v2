b2abd1cc72ace06c8776a7dff98612e405f536c6
mo7amedelfadil <mohamedelfadil1994@gmail.com>
2024-02-20 17:24:50 +0200

    update constraint for place's name

--- models/place.py
+++ models/place.py
@@ -13,7 +13,7 @@ from typing import List
                         nullable=False), ''),
         user_id=(Column(String(60), ForeignKey('users.id'),
                         nullable=False), ''),
-        name=(Column(String(128), nullable=True), ''),
+        name=(Column(String(128), nullable=False), ''),
         description=(Column(String(1024)), ''),
         number_rooms=(Column(Integer, nullable=False, default=0), 0),
         number_bathrooms=(Column(Integer, nullable=False, default=0), 0),
