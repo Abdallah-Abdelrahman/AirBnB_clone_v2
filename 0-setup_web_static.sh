06edfac1b6b40a808bfcea3d4a818e8853c3e856
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-07 02:26:36 +0300

    Rewrite server block

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,6 @@
 # script that sets up your web servers for the deployment of web_static
 
 LINK_NAME="/data/web_static/current"
-LOCATION="location /hbnb_static {\n\t\talias $LINK_NAME;\n\t}"
 
 # installed nginx if it's not installed already
 [[ ! $(dpkg-query -W nginx) ]] > /dev/null 2>&1 && apt update && apt install nginx -y
@@ -29,6 +28,37 @@ chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 # (ex: https://mydomainname.tech/hbnb_static
-sed -i "/pass PHP/a\\\t$LOCATION" /etc/nginx/sites-enabled/default
+cat << EOF > /etc/nginx/sites-enabled/default
+server {
+	add_header X-Served-By $(hostname);
 
-nginx -s reload
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	root /var/www/html;
+
+	# Add index.php to the list if you are using PHP
+	index index.html index.htm index.nginx-debian.html;
+
+	server_name _;
+
+	location / {
+		# First attempt to serve request as file, then
+		# as directory, then fall back to displaying a 404.
+		try_files \$uri \$uri/ =404;
+	}
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+	error_page 404 /404.html;
+        location = /404.html {
+		root /var/www/html;
+                internal;
+        }
+	location /hbnb_static {
+		alias /data/web_static/current;
+	}
+}
+EOF
+
+service nginx restart
