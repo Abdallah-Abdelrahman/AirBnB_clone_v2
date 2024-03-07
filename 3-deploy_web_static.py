af4b4416677ba85056451aa54a03b2fdba4ee5a2
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-08 02:21:05 +0300

    Bring all the tasks

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,8 +1,52 @@
 #!/usr/bin/python3
 '''Fabric script (based on the file 2-do_deploy_web_static.py) that creates
 and distributes an archive to your web servers, using the function deploy'''
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+from fabric.api import local, run, env, put
+from datetime import datetime
+from os.path import exists
+
+
+env.user = 'ubuntu'
+env.hosts = ['52.91.118.253', '35.153.16.72']
+
+
+def do_deploy(archive_path):
+    '''Distributes an archive to my web servers'''
+    if not exists(archive_path):
+        return False
+
+    try:
+        archive = archive_path.split('/')[-1].split('.')[0]
+        target = f'/data/web_static/releases/{archive}/'
+        # upload archive to /tmp/
+        put(archive_path, '/tmp/')
+        run(f'mkdir -p {target}')
+        run(f'tar -xzf /tmp/{archive}.tgz -C {target}')
+        run(f'rm /tmp/{archive}.tgz')
+        run(f'mv /data/web_static/releases/{archive}/web_static/* {target}')
+        run(f'rm -rf /data/web_static/releases/{archive}/web_static')
+        # remove symbolic link
+        run('rm -rf /data/web_static/current')
+        # create new link
+        run(f'ln -s {target} /data/web_static/current')
+        return True
+    except Exception:
+        return False
+
+
+def do_pack():
+    '''Generate .tgz archive from web_static dir'''
+    try:
+        local('mkdir -p versions')
+        time = datetime.now().strftime('%Y%m%d%H%M%S')
+        archive = 'web_static_' + time + '.tgz'
+
+        # generate archive in current directory
+        local(f'tar -cvzf versions/{archive} web_static')
+
+        return f'versions/{archive}'
+    except Exception:
+        return None
 
 
 def deploy():
