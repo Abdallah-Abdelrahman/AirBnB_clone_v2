e9e1292048f01fe07a676b744f0852c77622aac8
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-08 20:44:53 +0300

    Use `fabric.context_mangers`

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 '''Fabric script (based on the file 3-deploy_web_static.py)
 that deletes out-of-date archives, using the function do_clean'''
-from fabric.api import run, env, local
-from os import listdir
+from fabric.api import run, env, local, cd, lcd
 
 
 env.user = 'ubuntu'
@@ -12,14 +11,21 @@ env.key_filename = '~/.ssh/school'
 
 def do_clean(number=0):
     '''clean'''
-    res = listdir('versions')
     number = int(number)
-    versions = res[1:] if (number) <= 1 else res[number:]
-    # get archives sort from most recents
-    releases = run('ls -t /data/web_static/releases').strip().split()
-    rels = releases[1:] if (number) <= 1 else releases[number:]
+    try:
+        # context manger for local
+        with lcd('versions/'):
+            versions = local('ls -t versions/', capture=True).split()
+            versions = versions[1:] if number <= 1 else versions[number:]
+            for v in versions:
+                local(f'rm -rf versions/{v}')
 
-    for v in versions:
-        local(f'rm -rf versions/{v}')
-    for rel in rels:
-        run(f'rm -rf /data/web_static/releases/{rel}')
+        # context manger for remote
+        with cd('/data/web_static/releases'):
+            releases = run('ls -t /data/web_static/releases').split()
+            rels = releases[1:] if number <= 1 else releases[number:]
+            for rel in rels:
+                run(f'rm -rf /data/web_static/releases/{rel}')
+
+    except Exception:
+        pass
