c60847ee8874fc5eaa09c6a73c2b37ac7ed9aa41
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-09 12:30:24 +0300

    Add missing directories

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,5 +1,7 @@
 # puppet manifest
 package {'nginx': ensure => installed}
+file {'/data/': ensure => directory}
+file {'/data/web_static/': ensure => directory}
 file {'/data/web_static/releases': ensure => directory}
 file {'/data/web_static/shared': ensure => directory}
 file {'/data/web_static/releases/test/': ensure => directory}
