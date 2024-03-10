5e133d7c313673d148cd12d8bf37e7c1d5f15ff9
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-10 06:09:54 +0300

    Use `rsync` to copy files and preserve links and permissions

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,9 +5,8 @@ to your web servers, using the function do_deploy
 Attrs:
     env: enviroment variables for fabric
 '''
-from fabric.api import run, put, env, sudo, task
+from fabric.api import run, put, env
 from os.path import exists
-do_pack = __import__('1-pack_web_static').do_pack
 
 
 env.user = 'ubuntu'
@@ -15,7 +14,6 @@ env.hosts = ['52.91.118.253', '35.153.16.72']
 env.key_filename = '~/.ssh/school'
 
 
-@task(default=True)
 def do_deploy(archive_path):
     '''Distributes an archive to my web servers'''
     if not exists(archive_path):
@@ -25,12 +23,13 @@ def do_deploy(archive_path):
         # extract archive name w/out extension
         archive = archive_path.split('/')[-1].split('.')[0]
         target = f'/data/web_static/releases/{archive}'
+        opts = '-arctuxz --remove-source-files'
         # upload archive to /tmp/
         put(archive_path, '/tmp/')
-        sudo(f'mkdir -p {target}')
+        run(f'mkdir -p {target}')
         run(f'tar -xzf /tmp/{archive}.tgz -C {target}')
         run(f'rm /tmp/{archive}.tgz')
-        run(f'mv {target}/web_static/* {target}')
+        run(f'rsync {opts} {target}/web_static/ {target}')
         run(f'rm -rf {target}/web_static')
         # remove symbolic link
         run('rm -rf /data/web_static/current')
