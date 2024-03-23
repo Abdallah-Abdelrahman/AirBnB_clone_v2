117e0eae1b4d9e67c06e54b82a64287213d11ae3
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-23 21:14:32 +0300

    Add `docstring` for the module

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''starts a Flask web application'''
 from models import storage
 from models.state import State
 from flask import Flask, render_template
@@ -7,17 +8,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown_db(exception):
+    '''remove the current SQLAlchemy Session after each request
+    Args:
+        exception:
+    '''
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def state():
     '''list statues'''
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    '''remove the current SQLAlchemy Session after each request'''
-    storage.close()
 
 
 if __name__ == '__main__':
