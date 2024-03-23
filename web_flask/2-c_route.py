a6a37833bbc62356dd64ff97a79d05651a476f0c
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-23 04:26:32 +0300

    Add text route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''starts a Flask web application'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''hello route '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''hbnb route '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def txt(text):
+    '''text route '''
+    return text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
