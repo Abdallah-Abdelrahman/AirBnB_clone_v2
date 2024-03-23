71baf88b26b87178782f72eef3a9d36484cd84cc
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-23 04:33:20 +0300

    Update return value

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def txt(text):
     '''text route '''
-    return text.replace('_', ' ')
+    return 'C '+text.replace('_', ' ')
 
 
 if __name__ == '__main__':
