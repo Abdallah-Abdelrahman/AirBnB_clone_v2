a81784d7eb5026073b493d51dbcd4cac7d9cac2e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-23 04:41:30 +0300

    Add python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''starts a Flask web application'''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''hello route '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''hbnb route '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def txt(text):
+    '''text route '''
+    return 'C '+text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    '''text route '''
+    return 'Python '+text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
