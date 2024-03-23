8e82cad8186aae5ca334142c3be61180fd414dac
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-24 02:17:59 +0300

    Add route for a single state by id

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -26,4 +26,3 @@ def state_city():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,2 +1,35 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+'''starts a Flask web application'''
+from models import storage
+from models.state import State
+from flask import Flask, render_template
 
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    '''remove the current SQLAlchemy Session after each request
+    Args:
+        exception:
+    '''
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def state():
+    '''list statues'''
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_by_id(id):
+    '''get state by id'''
+    state = next((s for k, s in storage.all(State).items() if id == s.id), '')
+    return render_template('9-states.html', state=state)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
