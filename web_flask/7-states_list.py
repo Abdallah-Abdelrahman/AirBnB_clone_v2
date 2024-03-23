7fa181fd0421d75b5419b17828967d30d1a6ce88
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-23 20:20:59 +0300

    Define states route and teardown after each request

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def state():
+    '''list statues'''
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
